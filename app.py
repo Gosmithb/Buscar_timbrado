23762327bf83ceaef34e0436b3516bab6dc6117a
Gosmithb <bryangodinez00@gmail.com>
2025-06-24 09:14:44 -0700

    Se corrige validacion para asignar carpeta de destino

--- app.py
+++ app.py
@@ -34,10 +34,13 @@ def generar_pdf_filtrado(pagina, path_root):
   ruta_final = generar_nombre_unico(nombre_base)
 
   pdf_filtrado.save(ruta_final)
+  print(f"PDF guardado en {ruta_final}")
   pdf_filtrado.close()
 
 # Se selecciona pdf donde se buscara los datos ingresados
+print("Selecciona la carpeta de origen donde se encuentran los archivos PDF donde se realizará la búsqueda.")
 carpeta_origen = seleccionar_ruta("Seleccionar ruta de origen")
+print("Selecciona la carpeta de desstino donde se guardarán los archivos encontrados. (Se guardarán por defecto en carpeta 'Salida' en caso de no seleccionar alguna)")
 carpeta_salida = seleccionar_ruta("Seleccionar carpeta de destino")
 palabras_clave = input("Ingrese las palabras clave separadas por comas sin espacios: ").strip().split(',')
 
@@ -50,7 +53,11 @@ for root, _, files in os.walk(carpeta_origen):
         texto_pagina = pagina.get_text().lower()
         for palabra in palabras_clave:
           if palabra.strip().lower() in texto_pagina:
-            generar_pdf_filtrado(pagina.number, carpeta_salida)
+            if carpeta_salida == Path("."):
+              generar_pdf_filtrado(pagina.number, Path("Salida"))
+            else:
+              generar_pdf_filtrado(pagina.number, carpeta_salida)
+
             break
 
       archivo_actual.close()
